c06ca236abd0be379ae07153d0183ef9ac175f72
prat-degwekar <prat.degwekar@gmail.com>
2016-11-10 14:19:33 +0530

    Added BFS Tree Creation and DFS Tree creation

---
+++ bfs_tree.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int f=-1,r=-1;
+int q_n=0;
+
+void printit(int **fin , int n)
+{
+   int i=0 , j=0;
+   printf("final : \n");
+   for(i=0;i<n;i++)
+   {
+      for(j=0;j<n;j++)
+      {
+	 printf("%d ",fin[i][j]);
+      }
+      printf("\n");
+   }
+   printf("\n\n\n");
+}
+
+void bfs(int **arr , int *vis , int *q , int **fin)
+{
+   int fr=0 , t=0 , g=0;
+   int i=0,j=0;
+   for(i=0;i<q_n;i++)
+      vis[i]=0;
+    
+   q[0] = fr;
+   f=0;
+   r=0;
+
+   while(f<=r)
+   {
+      int k = q[f++];
+      for(i=0;i<q_n;i++)
+      {
+	 if(i==k)
+	    continue;
+	 if(arr[k][i]==1 && vis[i]==0)
+	 {
+	    printf("visiting %d\n\n",i);
+	    fin[k][i] = 1;
+	    fin[i][k] = 1;
+	    vis[i]=1;
+	    q[r++]=i;
+	    
+	 }//if
+      }
+   }//while
+
+   printf("peace\n\n");
+}//bfs
+
+
+void main()
+{
+   int i=0,j=0,k=0;
+   printf("enter no. of nodes : ");
+   scanf("%d",&q_n);
+   int *q = (int *)malloc(q_n*sizeof(int *));
+   int **arr = (int **)malloc(q_n*sizeof(int *));
+   int **fin = (int **)malloc(q_n*sizeof(int *));
+   for(i=0;i<q_n;i++)
+   {
+      arr[i] = (int *)malloc(q_n*sizeof(int));
+      fin[i] = (int *)malloc(q_n*sizeof(int));
+   }
+   for(i=0;i<q_n;i++)
+   {
+      for(j=0;j<q_n;j++)
+      {
+	 arr[i][j]=0;
+	 fin[i][j]=0;
+      }
+   }
+
+   int *vis = (int *)malloc(q_n*sizeof(int));
+
+   i=0;
+   while(i!=3)
+   {
+      printf("CHOOSE\n1 . ADD EDGE\n2 . BFS\n3 . EXIT : ");
+      scanf("%d",&i);
+      switch(i)
+      {
+	 case 1 : printf("enter nodes : ");
+		  scanf("%d %d",&j,&k);
+		  arr[j][k] = 1;
+		  arr[k][j] = 1;
+		  printf("added\n");
+		  break;
+
+	 case 2 : bfs(arr,vis,q,fin);
+		  printit(fin,q_n);
+		  break;
+
+	 case 3 : printf("you chose to exit!\n\n");
+		  break;
+
+	 default : printf("invalid choice\n\n");
+		   break;
+      }//switch(i)
+
+   }//while(i)
+
+   printf("GGWP\n\n");
+}//main()
---
+++ dfs_tree.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void printit(int **arr , int n)
+{
+   printf("\n\nmatrix\n\n");
+   int i=0,j=0;
+   for(i=0;i<n;i++)
+   {
+      for(j=0;j<n;j++)
+	 printf(" %d ",arr[i][j]);
+      printf("\n");
+   }
+   printf("\n\n\n");
+}
+
+void printvis(int *vis , int n)
+{
+   int i=0;
+   printf("\n\nvisited array : ");
+   for(i=0;i<n;i++)
+   {
+      printf(" %d ",vis[i]);
+   }
+   printf("\n\n");
+}//print visited
+
+int allvis(int *vis , int n)
+{
+   int i=0;
+   for(i=0;i<n;i++)
+   {
+      if(vis[i]==0)
+	 return 0;
+   }
+   return 1;
+}
+
+int fin[20][20];
+
+int dfs(int n , int **arr , int f , int t , int *vis,int **fin)
+{
+   printf("visiting : %d\n\n",f);
+   vis[f] = 1;
+   int i=0;
+   for(i=0;i<n;i++)
+   {
+      if(i==f)
+	 continue;
+      if(arr[f][i]==1 && vis[i] == 0)
+      {
+	 fin[f][i] = 1;
+	 fin[i][f] = 1;
+	 dfs(n,arr,i,t,vis,fin);
+      }
+   }
+   return 0;
+
+}//dfs(n,arr,f,t)
+
+void main()
+{
+   int n=0;
+   printf("enter the no. of nodes : ");
+   scanf("%d",&n);
+   int **arr = (int **)malloc(n*sizeof(int *));
+   int **fin = (int **)malloc(n*sizeof(int *));
+   int i=0;
+   for(i = 0 ; i<n; i++)
+   {
+      arr[i] = (int *)malloc(n*sizeof(int));
+      fin[i] = (int *)malloc(n*sizeof(int));
+   }
+   int j=0 , k=0 , g=0;
+   
+   int *vis = (int *)malloc(n*sizeof(int));
+
+   for(j=0;j<n;j++)
+      for(k=0;k<n;k++)
+	 arr[j][k] = 0;
+
+   j=0;
+   k=0;
+   i=0;
+   while(i!=3)
+   {
+      printf("ENTER\n1 . ADD EDGE\n2 . DFS\n3 . EXIT : ");
+      scanf("%d" , &i);
+      switch(i)
+      {
+	 case 1 : printf("enter the two nodes : ");
+		  scanf("%d %d",&j,&k);
+		  arr[j][k] = 1;
+		  arr[k][j] = 1;
+		  printf("added\n\n");
+		  break;
+
+	 case 2 : printit(arr,n);
+		  
+		  printf("enter nodes to find path between : ");
+		  scanf("%d %d",&j,&k);
+		  for(g=0;g<n;g++)
+		     vis[g]=0;
+		  vis[j] = 1;
+		  printf("\nhere safely\n\n");
+		  k = dfs(n,arr,j,k,vis,fin);
+		  //if(k==1) printf(" <-- %d\n",j);
+		  //printf("\n\nk = %d\n\n",k);
+		  printit(fin,n);
+		  break;
+
+	 case 3 : printf("you chose to quit.\n\n");
+		  break;
+
+	 default : printf("invalid choice\n\n");
+		   break;
+
+      }//switch(i)
+
+   }//while(i)
+
+   printf("GGWP\n\n");
+
+}//main()
+
